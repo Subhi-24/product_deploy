fb4e10f8a2f2a6f22bcd2cc6645a6be589c9980b
Subhi-24 <subhikshamj24@gmail.com>
2025-02-03 12:12:41 +0530

    Update ProductRoutes.js

--- Routes/ProductRoutes.js
+++ Routes/ProductRoutes.js
@@ -5,6 +5,8 @@ const router = require('express').Router()
 router.get('/api/products', productController.getAllProducts);
 router.get('/api/products/:id', productController.getSingleProduct)
 router.post('/api/product', productController.addNewProduct)
+router.put('/api/product', productController.updateProduct)
+router.delete('/api/product/:id', productController.deleteProduct)
 
 module.exports = router
 
